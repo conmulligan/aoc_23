62d8791779bba31373f6b25fa325472632179d29
Conor Mulligan <conmulligan@gmail.com>
2023-12-02 16:01:12 +0000

    Add day 2a

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use core::RunError;
 
-pub static TASKS: &[(&'static str, fn() -> Result<String, RunError>)] =
-    &[("day_1a", day_1a::run), ("day_1b", day_1b::run)];
+pub static TASKS: &[(&'static str, fn() -> Result<String, RunError>)] = &[
+    ("day_1a", day_1a::run),
+    ("day_1b", day_1b::run),
+    ("day_2a", day_2a::run),
+];
