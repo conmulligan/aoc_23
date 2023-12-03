c2e8aad539b37cf7deb8367cbfded08a804a292c
Conor Mulligan <conmulligan@gmail.com>
2023-12-03 22:21:08 +0000

    Clarify function name

--- day_3b/src/lib.rs
+++ day_3b/src/lib.rs
@@ -8,13 +8,13 @@ pub fn run() -> Result<String, RunError> {
     let lines = core::parse_lines(day_3a::INPUT);
     let grid = day_3a::parsing::parse_grid(lines);
     let part_numbers = day_3a::parsing::parse_part_numbers(&grid);
-    let gear_ratio = enumerate_gears(&part_numbers, &grid);
+    let gear_ratio = total_gear_ratio(&part_numbers, &grid);
 
     Ok(gear_ratio.to_string())
 }
 
-fn enumerate_gears(part_numbers: &Vec<PartNumber>, grid: &Grid) -> u32 {
-    let mut sum_gear_ratio: u32 = 0;
+fn total_gear_ratio(part_numbers: &Vec<PartNumber>, grid: &Grid) -> u32 {
+    let mut total_gear_ratio: u32 = 0;
 
     for (row_index, row) in grid.iter().enumerate() {
         for (col_index, char) in row.iter().enumerate() {
@@ -22,13 +22,13 @@ fn enumerate_gears(part_numbers: &Vec<PartNumber>, grid: &Grid) -> u32 {
                 let matches = adjacent_part_numbers(row_index, col_index, &part_numbers);
                 if matches.len() == 2 {
                     let ratio: u32 = matches.iter().product();
-                    sum_gear_ratio += ratio;
+                    total_gear_ratio += ratio;
                 }
             }
         }
     }
 
-    sum_gear_ratio
+    total_gear_ratio
 }
 
 fn adjacent_part_numbers(row: usize, col: usize, part_numbers: &Vec<PartNumber>) -> Vec<u32> {
