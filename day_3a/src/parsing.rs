15de2b452e7fbbf71f96b2773d113c0db279123d
Conor Mulligan <conmulligan@gmail.com>
2023-12-04 00:02:51 +0000

    Optimize part number parser

--- day_3a/src/parsing.rs
+++ day_3a/src/parsing.rs
@@ -1,5 +1,3 @@
-use std::ops::Range;
-
 use crate::model::{Grid, PartNumber};
 
 pub fn parse_grid(lines: Vec<&str>) -> Grid {
@@ -8,53 +6,38 @@ pub fn parse_grid(lines: Vec<&str>) -> Grid {
     rows
 }
 
-pub fn make_part_number(
-    grid: &Grid,
-    row_index: usize,
-    col_start_index: usize,
-    col_end_index: usize,
-) -> PartNumber {
-    let col_range = Range {
-        start: col_start_index,
-        end: col_end_index,
-    };
-    let value = value_of_range_grid(row_index, &col_range, grid);
-    PartNumber {
-        row_index,
-        col_range,
-        value,
-    }
-}
-
 pub fn parse_part_numbers(grid: &Grid) -> Vec<PartNumber> {
     let mut part_numbers: Vec<PartNumber> = Vec::new();
 
     for (row_index, row) in grid.iter().enumerate() {
-        let mut start_idx: Option<usize> = None;
+        let mut start_index: Option<usize> = None;
+        let mut current_digit: Option<u32> = None;
+
         for (col_index, char) in row.iter().enumerate() {
-            if char.is_ascii_digit() && start_idx.is_none() {
-                start_idx = Some(col_index);
-            } else if !char.is_ascii_digit() {
-                if let Some(start) = start_idx {
-                    part_numbers.push(make_part_number(grid, row_index, start, col_index));
-                    start_idx = None;
+            if char.is_ascii_digit() {
+                let digit = char.to_digit(10).unwrap();
+
+                if let Some(current) = current_digit {
+                    current_digit = Some((current * 10) + digit);
+                } else {
+                    start_index = Some(col_index);
+                    current_digit = Some(digit);
                 }
-            } else if col_index == row.len() - 1 {
-                if let Some(start) = start_idx {
-                    part_numbers.push(make_part_number(grid, row_index, start, col_index + 1));
+            }
+
+            if !char.is_ascii_digit() || col_index == row.len() - 1 {
+                if start_index.is_some() && current_digit.is_some() {
+                    part_numbers.push(PartNumber {
+                        row_index,
+                        col_range: start_index.unwrap()..col_index,
+                        value: current_digit.unwrap(),
+                    });
+                    start_index = None;
+                    current_digit = None;
                 }
             }
         }
     }
 
     part_numbers
-}
-
-fn value_of_range_grid(row_index: usize, range: &Range<usize>, grid: &Grid) -> u32 {
-    let row = &grid[row_index];
-    row[range.to_owned()]
-        .iter()
-        .collect::<String>()
-        .parse::<u32>()
-        .unwrap()
 }
